bb9c107a7eed07592e4c604ed40e22d9866f144a
Darya and Code story <daryaandcodestory@Fiodars-MacBook-Pro-4.local>
2024-04-29 16:57:44 -0400

    The query configuration object and aborting requests

--- src/util/http.js
+++ src/util/http.js
@@ -1,11 +1,12 @@
-export async function fetchEvents(serchTerm) {
+export async function fetchEvents({ signal, serchTerm }) {
+  console.log(serchTerm)
   let url =  'http://localhost:3000/events';
 
   if (serchTerm){
     url += '?search=' + serchTerm
   }
 
-  const response = await fetch('http://localhost:3000/events');
+  const response = await fetch(url, {signal: signal});
 
   if (!response.ok) {
     const error = new Error('An error occurred while fetching the events');
